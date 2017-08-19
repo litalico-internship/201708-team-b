6f17f4d39d8c4b69e979c76bd0bb7e5b18a784ff
Kosuke-Szk <brave.ksk@gmail.com>
2017-08-19 14:45:29 +0900

    Revert "必要データでユーザー登録ができるように修正"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
   # TODO: mail認証をする時にconfirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  validates :nickname, presence: true
-  validates :age, presence: true
-  validates :location, presence: true
-  validates :sexuality, presence: true
 
   has_many :direct_messages
 
