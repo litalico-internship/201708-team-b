870f93bc7c9da0215d4d4d72d5b20eecd0f18e40
Kosuke Suzuki <brave.ksk@gmail.com>
2017-08-19 16:58:10 +0900

    automatching機能の土台を作成

--- app/controllers/auto_matches_controller.rb
+++ app/controllers/auto_matches_controller.rb
@@ -1,3 +1,6 @@
 class AutoMatchesController < PermissionController
-  def show; end
+  def show
+    @user = current_user
+    @target_user = User.order("score desc").limit(1).where.not(id: current_user.id)[0]
+  end
 end
