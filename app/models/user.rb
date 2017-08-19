6f8537e03c1145ccd9dfd3a7f5a79853a3dae289
Kosuke Suzuki <brave.ksk@gmail.com>
2017-08-19 14:16:48 +0900

    signup機能を拡充しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   # TODO: mail認証をする時にconfirmable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :nickname, presence: true
+  validates :age, presence: true
+  validates :location, presence: true
+  validates :sexuality, presence: true
 end
