f54d7639a0666a4c62b2fb28d53f00e2e22231ac
ayuki_j <ajjt66554@gmail.com>
2017-08-19 18:31:18 +0900

    修正

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -6,7 +6,6 @@ class TestsController < ApplicationController
     value.each do |key, val|
       result += val.to_i
     end
-    # if Evaluation.evaluation(result)
     if result >= 7
       return redirect_to '/users/sign_up'
     end
