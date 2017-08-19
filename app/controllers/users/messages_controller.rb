7b945d3bd03be73b4f6f390783bffa56644871ad
竹本隼人 <takehaya1026@gmail.com>
2017-08-19 19:14:50 +0900

    modify controller

--- app/controllers/users/messages_controller.rb
+++ app/controllers/users/messages_controller.rb
@@ -3,7 +3,6 @@ module Users
     def index
       @receiver = User.find(params[:user_id])
       @messages = DirectMessage.where(receiver_id: @receiver.id, sender_id: current_user.id)
-      @message = DirectMessage.new
     end
   end
 end
