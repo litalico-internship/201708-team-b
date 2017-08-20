bd4eb5de799e3a02ce40a294830247506fa7d736
JijiiMac <haya199585@gmail.com>
2017-08-20 13:57:18 +0900

    fix_png

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
   prefectures = Constants::PREFECTURES
-  prefectures.each_with_index do |i, p|
+  prefectures.limit(40).each.with_index(50) do |i, p|
     User.create(name: 'seed', nickname: 'test', email: "#{p}@seed.com", age: '15', sexuality: 1, location: i[1], password: "#{p}pass")
   end
+
+  User.create(name: 'Takato Hayakawa', nickname: 'Takato', email: 'test@takato.com', age: '15', sexuality: 1, location: "Tokyo", password: 'password', icon_image: 'takato.jpg')
+  User.create(name: 'Kosuke Suzuki', nickname: 'Kosuke', email: 'test@kosuke.com', age: '15', sexuality: 1, location: "Kyoto", password: 'password', icon_image: 'suzu.jpg')
+  User.create(name: 'Joto Ayuki', nickname: 'Kansai', email: 'test@kansai.com', age: '15', sexuality: 1, location: "Osaka", password: 'password', icon_image: 'kansai.jpg')
+  User.create(name: 'Hayato Takemoto', nickname: 'Hayato', email: 'test@hayato.com', age: '15', sexuality: 1, location: "Tokyo", password: 'password', icon_image: 'take.jpg')
+  User.create(name: 'Yudai Tsukamoto', nickname: 'Yudai', email: 'test@yudai.com', age: '15', sexuality: 1, location: "Nagoya", password: 'password', icon_image: 'mentor.jpg')
