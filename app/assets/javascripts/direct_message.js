c62b49bca7e897d7fd8b0072ea814b1b227c830c
竹本隼人 <takehaya1026@gmail.com>
2017-08-20 10:34:56 +0900

    turbolinks

--- app/assets/javascripts/direct_message.js
+++ app/assets/javascripts/direct_message.js
@@ -1,36 +1,41 @@
 $(function() {
   function appendMessage(message) {
+    console.log('gsdfd');
     var html = buildMessageHtml(message);
     $('.messages_list').append(html);
   }
 
   function buildMessageHtml(message) {
+    console.log('ddd');
     var message = "<div class='each_message'><div class='message_text'>" + message['text'] + "</div></div>";
     return message;
   }
 
-  $('#new_direct_message').on('submit', function(ev) {
-    ev.preventDefault();
-    var $textField = $('#message_field');
-    var inputText = $textField.val();
+  $(document).on('turbolinks:load', function(){
+    $('#new_direct_message').on('submit', function(ev) {
+      console.log('dfaew');
+      ev.preventDefault();
+      var $textField = $('#message_field');
+      var inputText = $textField.val();
 
-    $.ajax({
-      type: 'POST',
-      url: './direct_messages',
-      dataType: 'json',
-      data: {
-        direct_message:
-          {
-            text: inputText,
-          }
-      },
-      context: this
-    })
-    .done(function(data) {
-      appendMessage(data);
-      this.reset();
+      $.ajax({
+        type: 'POST',
+        url: './direct_messages',
+        dataType: 'json',
+        data: {
+          direct_message:
+            {
+              text: inputText,
+            }
+        },
+        context: this
+      })
+      .done(function(data) {
+        appendMessage(data);
+        this.reset();
+      });
+
+      return false;
     });
-
-    return false;
   });
 });
