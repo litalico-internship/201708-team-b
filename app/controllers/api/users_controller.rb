85e0e5e9d48cd6ee2976308d061a967081fe9d1b
JijiiMac <haya199585@gmail.com>
2017-08-20 13:15:25 +0900

    permission_for_users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def get_all_users
-    users = User.where.not("id = ? AND location = ?", current_user.id, nil)
+    users = current_user.present? ? User.where.not("id = ? AND location = ?", current_user.id, nil) : User.where.not(location: nil)
     users.to_json(only: [:id, :nickname, :latitude, :longitude])
     render json: users, status: :ok
   end
