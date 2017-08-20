7b41f2d8965ea53c606377f77d096092bb39cb35
Kosuke Suzuki <brave.ksk@gmail.com>
2017-08-20 12:45:50 +0900

    routesの変更が不要だったので修正

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   resources :tests, only: [:index,:create]
   resources :top, only: [:index]
-  resources :auto_matches, only: [:show], as: :auto_match
+  resources :auto_matches, only: [:show]
   resources :users do
     resources :likes, only: [:create, :destroy]
   end
