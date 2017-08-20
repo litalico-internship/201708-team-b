81acb155d6664a1d74591ea9c14a103189ed24e3
JijiiMac <haya199585@gmail.com>
2017-08-20 12:51:41 +0900

    fix_users

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::UsersController < ApplicationController
   def get_all_users
-    users = User.where.not(location: nil)
+    users = User.where.not("id = ? AND location = ?", current_user.id, nil)
     users.to_json(only: [:id, :nickname, :latitude, :longitude])
     render json: users, status: :ok
   end
