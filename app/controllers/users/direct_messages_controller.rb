4fd0ee69e71936d78fe9df0a9bf8f4444075d17d
竹本隼人 <takehaya1026@gmail.com>
2017-08-20 10:06:30 +0900

    api作成

---
+++ app/controllers/users/direct_messages_controller.rb
@@ -0,0 +1,32 @@
+module Users
+  class DirectMessagesController < ApplicationController
+    before_action :set_context
+
+    def index
+      @message = DirectMessage.new
+    end
+
+    def create
+      @message = DirectMessage.new(create_params)
+      if @message.save
+        render json: @message.to_json
+      else
+        flash.now[:alert] = 'メッセージの送信に失敗しました。'
+        render :index
+      end
+    end
+
+    private
+
+    def create_params
+      params.require(:direct_message).permit(:text).merge(receiver_id: params[:user_id], sender_id: current_user.id)
+    end
+
+    def set_context
+      @partner = User.find(params[:user_id])
+      @messages = DirectMessage
+                    .where(sender_id: [current_user.id, @partner.id], receiver_id: [current_user.id, @partner.id])
+                    .order(:created_at)
+    end
+  end
+end
